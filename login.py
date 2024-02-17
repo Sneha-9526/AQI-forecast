f4a755778124d4f49fa802b60e9c27bd160447d2
Sneha-9526 <snehahari46@gmail.com>
2024-02-17 12:07:41 +0530

    first commit

---
+++ login.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def main():
+    st.title("Login Page")
+    username = st.text_input("User")
+    password = st.text_input("Password", type="password")
+
+    
+    if st.button("Login"):
+        if username == "your_username" and password == "your_password":
+            st.success("Login successful!")
+            # Add code for the page after successful login
+        else:
+            st.error("Invalid username or password")
+
+if __name__ == "__main__":
+    main()
